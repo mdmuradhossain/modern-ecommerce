c276be1917828eea01620266f0eacab2273ef529
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-28 12:44:11 +0600

    dto package added

--- src/main/java/io/murad/modern/ecommerce/config/WebSecurityConfig.java
+++ src/main/java/io/murad/modern/ecommerce/config/WebSecurityConfig.java
@@ -2,19 +2,13 @@ package io.murad.modern.ecommerce.config;
 
 import io.murad.modern.ecommerce.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 
 @Configuration
@@ -23,7 +17,7 @@ import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userDetailsService;
-
+    private final PasswordEncoderImpl passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,23 +31,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
-        .passwordEncoder(passwordEncoder());
+        .passwordEncoder(passwordEncoder.passwordEncoder());
 
     }
 
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public SCryptPasswordEncoder sCryptPasswordEncoder() {
-//        return new SCryptPasswordEncoder();
-//    }
 }
--- src/main/java/io/murad/modern/ecommerce/service/UserService.java
+++ src/main/java/io/murad/modern/ecommerce/service/UserService.java
@@ -1,17 +1,14 @@
 package io.murad.modern.ecommerce.service;
 
-import io.murad.modern.ecommerce.database.model.CustomUserDetails;
 import io.murad.modern.ecommerce.database.model.User;
 import io.murad.modern.ecommerce.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.ArrayList;
 
 @Service
 @AllArgsConstructor
@@ -20,12 +17,14 @@ public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 //        Supplier<UsernameNotFoundException> s =
 //                () -> new UsernameNotFoundException(
 //                        "Problem during authentication!");
 
         User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username Not Found " + username));
-        return new CustomUserDetails(user);
+//        return new CustomUserDetails(user);
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),new ArrayList<>());
     }
 }
