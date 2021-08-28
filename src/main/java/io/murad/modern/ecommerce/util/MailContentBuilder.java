36a5effa782cd2c181ebae71442c693d00efc92a
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-28 14:36:48 +0600

    MailContentBuilder added

---
+++ src/main/java/io/murad/modern/ecommerce/util/MailContentBuilder.java
@@ -0,0 +1,19 @@
+package io.murad.modern.ecommerce.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Component
+public class MailContentBuilder {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    public String build(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        return templateEngine.process("mailTemplate", context);
+    }
+}
