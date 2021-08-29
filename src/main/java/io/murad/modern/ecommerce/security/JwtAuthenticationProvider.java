178643bf8fef470cc88561f982bdd3dd4f9b8154
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-29 12:52:39 +0600

    generateJwtToken method added

--- src/main/java/io/murad/modern/ecommerce/security/JwtAuthenticationProvider.java
+++ src/main/java/io/murad/modern/ecommerce/security/JwtAuthenticationProvider.java
@@ -1,6 +1,10 @@
 package io.murad.modern.ecommerce.security;
 
+import io.jsonwebtoken.Jwts;
 import io.murad.modern.ecommerce.exception.ModernEcommerceException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,11 +15,16 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.time.Instant;
+import java.util.Date;
 
 @Service
 public class JwtAuthenticationProvider {
 
     private KeyStore keyStore;
+
+    @Value("900000")
+    private Long jwtExpirationInMillis;
 
     @PostConstruct
     public void init() {
@@ -27,5 +36,15 @@ public class JwtAuthenticationProvider {
             e.printStackTrace();
             throw new ModernEcommerceException("Exception occurred while loading keystore..." + e.getMessage());
         }
+    }
+
+    public String generateJwtToken(Authentication authentication) {
+        User principal = (User) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
+                .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
+                .compact();
     }
 }
