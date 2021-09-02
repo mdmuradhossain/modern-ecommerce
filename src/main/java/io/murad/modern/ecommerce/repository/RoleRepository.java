8d0fe1e1d2d2c0a2cfc3fa7066b85771d5130dfe
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-02 14:51:55 +0600

    RoleRepository added

---
+++ src/main/java/io/murad/modern/ecommerce/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package io.murad.modern.ecommerce.repository;
+
+import io.murad.modern.ecommerce.database.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+}
