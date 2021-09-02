1e1ed0a103e033428be49d8c9376ae8afce61a69
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-02 19:52:48 +0600

    addUserOrAdmin , getUser ,getUsers method added to UserService

--- src/main/java/io/murad/modern/ecommerce/service/UserService.java
+++ src/main/java/io/murad/modern/ecommerce/service/UserService.java
@@ -1,9 +1,13 @@
 package io.murad.modern.ecommerce.service;
 
+import io.murad.modern.ecommerce.database.model.User;
 import io.murad.modern.ecommerce.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -11,4 +15,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public void addUserOrAdmin(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
 }
