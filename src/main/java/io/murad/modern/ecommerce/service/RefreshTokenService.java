b09c5d7d1d881e172f86f14e0ee61837281f3406
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-02 12:49:39 +0600

    generateRefreshToken method added to RefreshTokenService

---
+++ src/main/java/io/murad/modern/ecommerce/service/RefreshTokenService.java
@@ -0,0 +1,27 @@
+package io.murad.modern.ecommerce.service;
+
+import io.murad.modern.ecommerce.database.model.RefreshToken;
+import io.murad.modern.ecommerce.repository.RefreshTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken generateRefreshToken() {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setCreatedDate(Instant.now());
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+}
