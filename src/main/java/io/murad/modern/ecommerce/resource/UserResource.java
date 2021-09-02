8fae56bbfd6b06729a636d27e34cc4f103e2a084
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-02 19:54:21 +0600

    UserReource added

---
+++ src/main/java/io/murad/modern/ecommerce/resource/UserResource.java
@@ -0,0 +1,11 @@
+package io.murad.modern.ecommerce.resource;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/users")
+public class UserResource {
+}
