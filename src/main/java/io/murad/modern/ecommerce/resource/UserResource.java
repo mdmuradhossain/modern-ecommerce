65b2b269a0d1854ffe66364659847374909750b0
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-02 19:57:05 +0600

    createUser method added to UserResource

--- src/main/java/io/murad/modern/ecommerce/resource/UserResource.java
+++ src/main/java/io/murad/modern/ecommerce/resource/UserResource.java
@@ -1,6 +1,11 @@
 package io.murad.modern.ecommerce.resource;
 
+import io.murad.modern.ecommerce.database.model.User;
+import io.murad.modern.ecommerce.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping(path = "/users")
 public class UserResource {
+
+    private final UserService userService;
+
+    @PostMapping()
+    public ResponseEntity<?> createUser(User user) {
+        userService.addUserOrAdmin(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
