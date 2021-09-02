48a6511176938bc05be73a586e7d0324102d7b57
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-02 19:58:41 +0600

    getUsers method added to UserReource

--- src/main/java/io/murad/modern/ecommerce/resource/UserResource.java
+++ src/main/java/io/murad/modern/ecommerce/resource/UserResource.java
@@ -5,9 +5,12 @@ import io.murad.modern.ecommerce.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -20,5 +23,10 @@ public class UserResource {
     public ResponseEntity<?> createUser(User user) {
         userService.addUserOrAdmin(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
     }
 }
