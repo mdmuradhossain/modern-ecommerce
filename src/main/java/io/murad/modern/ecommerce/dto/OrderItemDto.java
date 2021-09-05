1b0b940c54c7fa1e4a3f872cc7378cd9b837d39b
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-05 19:38:02 +0600

    OrderItemDto added

--- src/main/java/io/murad/modern/ecommerce/dto/OrderItemDto.java
+++ src/main/java/io/murad/modern/ecommerce/dto/OrderItemDto.java
@@ -4,15 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class OrderDto {
+public class OrderItemDto {
 
-    private Long id;
-    private Long userId;
-//    private Instant createdDate;
-//    private Double totalOrderPrice;
+    private Double price;
+    private Integer quantity;
+    private Long orderId;
+    private Long productId;
+    
 }
