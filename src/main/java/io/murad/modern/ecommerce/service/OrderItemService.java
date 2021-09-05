8a22f8036c28053fdad4e822ed6e1b38a13fd619
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-05 19:35:00 +0600

    OrderItemService added

---
+++ src/main/java/io/murad/modern/ecommerce/service/OrderItemService.java
@@ -0,0 +1,11 @@
+package io.murad.modern.ecommerce.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class OrderItemService {
+}
