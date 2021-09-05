4413c6eb51b05b4543021fa57d5b00c81cf3c618
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-05 19:42:13 +0600

    CartDto added

---
+++ src/main/java/io/murad/modern/ecommerce/dto/CartDto.java
@@ -0,0 +1,15 @@
+package io.murad.modern.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartDto {
+    private List<CartItemDto> cartItems;
+    private double totalPrice;
+}
