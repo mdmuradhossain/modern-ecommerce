4b6b5057adc0acd227bdef7ebe93a2f58e6f2c2f
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-05 19:50:44 +0600

    addOrderedProducts method added

--- src/main/java/io/murad/modern/ecommerce/service/OrderItemService.java
+++ src/main/java/io/murad/modern/ecommerce/service/OrderItemService.java
@@ -1,5 +1,7 @@
 package io.murad.modern.ecommerce.service;
 
+import io.murad.modern.ecommerce.database.model.OrderItem;
+import io.murad.modern.ecommerce.repository.OrderItemRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,9 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Slf4j
 public class OrderItemService {
+    private final OrderItemRepository orderItemRepository;
+
+    public void addOrderedProducts(OrderItem orderItem){
+        orderItemRepository.save(orderItem);
+    }
 }
