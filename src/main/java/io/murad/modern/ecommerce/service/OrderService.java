4b4d6388f17f01ab9af06d81a4777257ed6ab076
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-06 12:41:44 +0600

    createSessionLineItem and createSession method added

--- src/main/java/io/murad/modern/ecommerce/service/OrderService.java
+++ src/main/java/io/murad/modern/ecommerce/service/OrderService.java
@@ -1,5 +1,8 @@
 package io.murad.modern.ecommerce.service;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 import io.murad.modern.ecommerce.database.model.Currency;
 import io.murad.modern.ecommerce.database.model.Order;
@@ -14,9 +17,11 @@ import io.murad.modern.ecommerce.repository.CartRepository;
 import io.murad.modern.ecommerce.repository.OrderItemRepository;
 import io.murad.modern.ecommerce.repository.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,6 +35,12 @@ public class OrderService {
     private final OrderItemRepository orderItemRepository;
     private final CartService cartService;
     private final OrderItemService orderItemService;
+
+    @Value("${baseUrl}")
+    private final String baseUrl;
+
+    @Value("${STRIPE_SECRET_KEY}")
+    private final String apiKey;
 
     public Order saveOrder(PlaceOrderDto orderDto, User user, String sessionID) {
         Order order = getOrderFromDto(orderDto, user, sessionID);
@@ -86,7 +97,33 @@ public class OrderService {
                 .build();
     }
 
-    SessionCreateParams.LineItem createSessionItem(CheckoutItemDto checkoutItemDto){
+    SessionCreateParams.LineItem createSessionLineItem(CheckoutItemDto checkoutItemDto) {
+        return SessionCreateParams.LineItem.builder()
+                .setPriceData(createPriceData(checkoutItemDto))
+                .setQuantity(Long.parseLong(String.valueOf(checkoutItemDto.getQuantity())))
+                .build();
+    }
 
+    public Session createSession(List<CheckoutItemDto> checkoutItemDtoList) throws StripeException {
+
+        String successUrl = baseUrl + "/payment/success";
+        String failureUrl = baseUrl + "/payment/failure";
+
+        Stripe.apiKey = apiKey;
+
+        List<SessionCreateParams.LineItem> sessionItemList = new ArrayList<SessionCreateParams.LineItem>();
+        for(CheckoutItemDto checkoutItemDto: checkoutItemDtoList){
+        sessionItemList.add(createSessionLineItem(checkoutItemDto));
+        }
+//        checkoutItemDtoList.forEach(checkoutItemDto -> sessionItemList.add(createSessionLineItem(checkoutItemDto)));
+
+        SessionCreateParams params = SessionCreateParams.builder()
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setCancelUrl(failureUrl)
+                .addAllLineItem(sessionItemList)
+                .setSuccessUrl(successUrl)
+                .build();
+        return Session.create(params);
     }
 }
