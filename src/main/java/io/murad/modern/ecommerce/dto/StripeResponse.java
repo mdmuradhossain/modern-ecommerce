2f70a9dc422fea6c8f8f5f328e733b71cf8b4816
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-06 13:44:39 +0600

    checkoutList method added

---
+++ src/main/java/io/murad/modern/ecommerce/dto/StripeResponse.java
@@ -0,0 +1,12 @@
+package io.murad.modern.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StripeResponse {
+    private String sessionId;
+}
--- src/main/java/io/murad/modern/ecommerce/resource/OrderResource.java
+++ src/main/java/io/murad/modern/ecommerce/resource/OrderResource.java
@@ -1,16 +1,19 @@
 package io.murad.modern.ecommerce.resource;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
 import io.murad.modern.ecommerce.database.model.User;
+import io.murad.modern.ecommerce.dto.CheckoutItemDto;
+import io.murad.modern.ecommerce.dto.StripeResponse;
 import io.murad.modern.ecommerce.service.AuthService;
 import io.murad.modern.ecommerce.service.OrderService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -28,5 +31,10 @@ public class OrderResource {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-
+    @PostMapping("/create-checkout-session")
+    public ResponseEntity<StripeResponse> checkoutList(@RequestBody List<CheckoutItemDto> checkoutItemDtoList) throws StripeException {
+        Session session = orderService.createSession(checkoutItemDtoList);
+        StripeResponse stripeResponse = new StripeResponse(session.getId());
+        return new ResponseEntity<StripeResponse>(stripeResponse,HttpStatus.OK);
+    }
 }
