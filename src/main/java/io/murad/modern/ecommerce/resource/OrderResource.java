30377efcd42f46f2548f8a317fb071cbf8a50719
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-06 14:44:49 +0600

    checkout success

--- src/main/java/io/murad/modern/ecommerce/resource/OrderResource.java
+++ src/main/java/io/murad/modern/ecommerce/resource/OrderResource.java
@@ -6,12 +6,14 @@ import io.murad.modern.ecommerce.database.model.Order;
 import io.murad.modern.ecommerce.database.model.User;
 import io.murad.modern.ecommerce.dto.CheckoutItemDto;
 import io.murad.modern.ecommerce.dto.StripeResponse;
+import io.murad.modern.ecommerce.repository.UserRepository;
 import io.murad.modern.ecommerce.service.AuthService;
 import io.murad.modern.ecommerce.service.OrderService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,11 +25,11 @@ import java.util.List;
 public class OrderResource {
 
     private final OrderService orderService;
-    private final AuthService authService;
+    private final UserRepository userRepository;
 
     @PostMapping()
-    public ResponseEntity<?> placeOrder(@RequestParam("sessionId") String sessionId) {
-        User user = authService.getCurrentUser();
+    public ResponseEntity<?> placeOrder(@RequestParam("sessionId") String sessionId, Authentication authentication) {
+        User user = userRepository.findByUsername(authentication.getName()).get();
         orderService.checkOut(user, sessionId);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
@@ -40,9 +42,8 @@ public class OrderResource {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Order>> getAllOrders() {
-
-        User user = authService.getCurrentUser();
+    public ResponseEntity<List<Order>> getAllOrders(Authentication authentication) {
+        User user = userRepository.findByUsername(authentication.getName()).get();
         List<Order> orderDtoList = orderService.listOrders(user);
         return new ResponseEntity<List<Order>>(orderDtoList, HttpStatus.OK);
     }
