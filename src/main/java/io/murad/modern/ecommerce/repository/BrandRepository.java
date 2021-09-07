4d9778216ba9dd73be8775d3fc8b2d55d14a12ac
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-07 14:14:47 +0600

    BrandRepository added

---
+++ src/main/java/io/murad/modern/ecommerce/repository/BrandRepository.java
@@ -0,0 +1,7 @@
+package io.murad.modern.ecommerce.repository;
+
+import io.murad.modern.ecommerce.database.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand,Long> {
+}
