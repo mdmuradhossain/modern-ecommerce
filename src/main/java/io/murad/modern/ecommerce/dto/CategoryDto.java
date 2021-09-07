43d0ef250028e6e1ad5142c191d3d97f315fea43
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-07 12:15:25 +0600

    CategoryMapper added

--- src/main/java/io/murad/modern/ecommerce/dto/CategoryDto.java
+++ src/main/java/io/murad/modern/ecommerce/dto/CategoryDto.java
@@ -12,5 +12,4 @@ public class CategoryDto {
     private Long id;
     private String name;
     private String description;
-    private Long productId;
 }
---
+++ src/main/java/io/murad/modern/ecommerce/mapper/CategoryMapper.java
@@ -0,0 +1,20 @@
+package io.murad.modern.ecommerce.mapper;
+
+import io.murad.modern.ecommerce.database.model.Category;
+import io.murad.modern.ecommerce.dto.CategoryDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    public Category mapToCategory(CategoryDto categoryDto);
+
+
+
+    @Mapping(target = "id",source="category.id")
+    @Mapping(target = "name",source="category.categoryName")
+    @Mapping(target = "description",source="category.description")
+    @Mapping(target="products",ignore = true)
+    public CategoryDto mapDtoToCategory(Category category);
+}
