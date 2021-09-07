c24c704c60bef7127c0e041757fd6364c77e0a71
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-07 12:06:13 +0600

    CategoryDto added

---
+++ src/main/java/io/murad/modern/ecommerce/dto/CategoryDto.java
@@ -0,0 +1,16 @@
+package io.murad.modern.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Long productId;
+}
