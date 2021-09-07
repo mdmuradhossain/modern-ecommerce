ca7b1fc98818ec50e136e5b7462941efb461ab22
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-07 12:49:09 +0600

    CategoryResource added with GET,POST,PUT,DELETE request

---
+++ src/main/java/io/murad/modern/ecommerce/resource/CategoryResource.java
@@ -0,0 +1,48 @@
+package io.murad.modern.ecommerce.resource;
+
+import io.murad.modern.ecommerce.dto.CategoryDto;
+import io.murad.modern.ecommerce.service.CategoryService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "/categories")
+public class CategoryResource {
+    private final CategoryService categoryService;
+
+    @PostMapping()
+    public ResponseEntity<?> createCategory(@RequestBody CategoryDto categoryDto) {
+        categoryService.addCategory(categoryDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<CategoryDto> getCategory(@PathVariable("id") Long id) {
+        CategoryDto categoryDto = categoryService.getCategory(id);
+        return new ResponseEntity<>(categoryDto, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<CategoryDto>> getCategories() {
+        return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> editCategory(@PathVariable("id") Long id) {
+        categoryService.updateCategory(id);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeCategory(@PathVariable("id") Long id) {
+        categoryService.deleteCategory(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
