c371988a04303d7f285457641b31cc7ecbc69b46
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-07 14:15:52 +0600

    BrandDto added

---
+++ src/main/java/io/murad/modern/ecommerce/dto/BrandDto.java
@@ -0,0 +1,16 @@
+package io.murad.modern.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BrandDto {
+
+    private Long id;
+    private String name;
+    private String logo;
+    private String description;
+}
