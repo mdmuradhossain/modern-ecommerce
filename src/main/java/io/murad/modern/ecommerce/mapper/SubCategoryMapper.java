efd9b635486ed57df15e4c21cd1c89bc82d2d800
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-08 12:57:07 +0600

    SubCategoryMapper added

---
+++ src/main/java/io/murad/modern/ecommerce/mapper/SubCategoryMapper.java
@@ -0,0 +1,20 @@
+package io.murad.modern.ecommerce.mapper;
+
+import io.murad.modern.ecommerce.database.model.Category;
+import io.murad.modern.ecommerce.database.model.SubCategory;
+import io.murad.modern.ecommerce.dto.SubCategoryDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface SubCategoryMapper {
+
+
+    @Mapping(target = "subCategoryName", source = "subCategoryDto.name")
+    @Mapping(target = "category",source = "category")
+    public SubCategory mapToSubCategory(SubCategoryDto subCategoryDto, Category category);
+
+    @Mapping(target = "name", source = "subCategory.subCategoryName")
+    public SubCategoryDto mapSubCategoryToDto(SubCategory subCategory);
+
+}
