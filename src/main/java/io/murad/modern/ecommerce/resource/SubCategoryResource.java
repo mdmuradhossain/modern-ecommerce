f75b31376c29c634127d1b61685ea293cbbc8149
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-08 14:05:53 +0600

    SubCategoryResource added with GET,POST,PUT,DELETE request

---
+++ src/main/java/io/murad/modern/ecommerce/resource/SubCategoryResource.java
@@ -0,0 +1,47 @@
+package io.murad.modern.ecommerce.resource;
+
+import io.murad.modern.ecommerce.dto.SubCategoryDto;
+import io.murad.modern.ecommerce.service.SubCategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/subcategories")
+@AllArgsConstructor
+public class SubCategoryResource {
+
+    private final SubCategoryService subCategoryService;
+
+    @PostMapping()
+    public ResponseEntity<?> createSubCategory(@RequestBody SubCategoryDto subCategoryDto) {
+        subCategoryService.addSubCategory(subCategoryDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<SubCategoryDto> getSubCategory(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(subCategoryService.getSubCategory(id), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<SubCategoryDto>> getSubCategories() {
+        return new ResponseEntity<>(subCategoryService.getAllSubCategories(), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> editSubCategory(@PathVariable("id") Long id, @RequestBody SubCategoryDto subCategoryDto) {
+        subCategoryService.updateSubCategory(id, subCategoryDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> removeSubCategory(@PathVariable("id") Long id) {
+        subCategoryService.deleteSubCategory(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
