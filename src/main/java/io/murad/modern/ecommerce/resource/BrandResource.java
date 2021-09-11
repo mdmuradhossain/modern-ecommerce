f778b006fa959708ac7f0370f1b74a804ae088ce
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-11 15:26:04 +0600

    brand api endpoints created to BrandResource with GET,PUT,DELETE method

--- src/main/java/io/murad/modern/ecommerce/resource/BrandResource.java
+++ src/main/java/io/murad/modern/ecommerce/resource/BrandResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @AllArgsConstructor
@@ -23,5 +24,28 @@ public class BrandResource {
     public ResponseEntity<?> createBrand(@RequestBody BrandDto brandDto, @RequestParam("file") MultipartFile file) throws IOException {
         brandService.addBrand(brandDto, file);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<BrandDto> getBrand(@PathVariable("id") Long id) {
+        BrandDto brandDto = brandService.getBrand(id);
+        return new ResponseEntity<>(brandDto, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<BrandDto>> allBrands() {
+        return new ResponseEntity<>(brandService.getBrands(), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> editBrand(@PathVariable("id") Long id, @RequestBody BrandDto brandDto) {
+        brandService.updateBrand(id, brandDto);
+        return new ResponseEntity<>("Brand Updated.", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> removeBrand(@PathVariable("id") Long id) {
+        brandService.deleteBrand(id);
+        return new ResponseEntity<>("Brand Removed.", HttpStatus.OK);
     }
 }
